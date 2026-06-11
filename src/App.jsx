390a8896bb4c4619fbc04f3e2d5db46607f662e5
Vinícius dos Santos Oliveira <3izuna@gmail.com>
2026-06-10 22:37:23 -0300

    comentando algumas classes que aprendi

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,17 @@ function App() {
   return (
     <>
     <h1 className="ml-15 text-black-600 text-xl">Hello World</h1>
+    <button className='text-black-600 bg-gray-400 p-3 rounded-md hover:bg-gray-500 hover:text-white'>Testing</button>
       
     </>
   )
 }
 
 export default App
+
+
+// cor de texto e peso - text-color-weight
+// padding - p-rem
+// margin - m-rem
+// background-color - bg-color-intensity
+// border-radius - rounded-size
