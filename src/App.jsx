0ca839406b8f6a2f4631b2791ac9d20aa9892b4f
Vinícius dos Santos Oliveira <3izuna@gmail.com>
2026-06-10 22:12:10 -0300

    Instalando a extensão tailwind css intelliSense

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
 
   return (
     <>
-    <h1 className='text-3xl underline'>Hello World</h1>
+    <h1 className="ml-15 text-black-600 text-xl">Hello World</h1>
       
     </>
   )
