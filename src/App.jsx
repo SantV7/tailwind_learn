61ec97835a069c4b2e452b6bf1039332a68dcc58
Vinícius dos Santos Oliveira <3izuna@gmail.com>
2026-06-10 23:22:11 -0300

    Por hoje já deu, irei estudar mais amanhã, deu pra conhecer o que é o tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,39 @@ function App() {
   return (
     <>
     <h1 className="ml-15 text-black-600 text-xl">Hello World</h1>
-    <button className='text-black-600 bg-gray-400 p-3 rounded-md hover:bg-gray-500 hover:text-white'>Testing</button>
+  
+    <button
+     className=' 
+     ml-2 py-2 px-7
+     bg-blue-300
+     text-black-600 rounded-sm
+     hover:bg-blue-400 cursor-pointer
+     hover:rounded-xl 
+    '>
+      BTN Add
+    </button>
+
+    <button
+     className=' 
+     ml-2 py-2 px-7
+     bg-red-500
+     text-black-600 rounded-sm
+     hover:bg-red-600 cursor-pointer
+     hover:rounded-xl 
+    '>
+      BTN Cancel
+    </button>
+
+    <button
+     className=' 
+     ml-2 py-3 px-7
+     bg-gray-400
+     text-black-600 rounded-sm
+     hover:bg-gray-400 cursor-pointer
+     hover:rounded-xl 
+    '>
+      BTN Show
+    </button>    
       
     </>
   )
